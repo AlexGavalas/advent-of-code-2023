edd78c5bbd2cf9082f5652850afc3fdf7c909785
Alex Gavalas <alex.gavalas.92@gmail.com>
2023-12-09 23:08:20 +0200

    feat: day 9

--- src/day-9/src/main.rs
+++ src/day-9/src/main.rs
@@ -4,16 +4,16 @@ mod part_2;
 use utils::read_lines;
 
 fn get_input_file_path() -> &'static str {
-    "./src/day-8/src/input.txt"
+    "./src/day-9/src/input.txt"
 }
 
 fn main() {
-    println!("Hello, day 8!");
+    println!("Hello, day 9!");
 
     let lines = read_lines(get_input_file_path());
 
-    println!("Part 1: (19631) {}", part_1::solve(&lines));
-    println!("Part 2: () {}", part_2::solve(&lines));
+    println!("Part 1: (1882395907) {}", part_1::solve(&lines));
+    println!("Part 2: (1005) {}", part_2::solve(&lines));
 }
 
 #[cfg(test)]
@@ -30,18 +30,18 @@ mod tests {
     fn test_part_1() {
         let lines = read_lines(get_test_input_file_path());
 
-        assert_eq!(19631, part_1::solve(&lines));
+        assert_eq!(1882395907, part_1::solve(&lines));
     }
 
     #[test]
     fn test_part_2() {
         let lines = read_lines(get_test_input_file_path());
 
-        assert_eq!(0, part_2::solve(&lines));
+        assert_eq!(1005, part_2::solve(&lines));
     }
 
     #[test]
     fn test_get_input_file_path() {
-        assert_eq!("./src/day-8/src/input.txt", get_input_file_path());
+        assert_eq!("./src/day-9/src/input.txt", get_input_file_path());
     }
 }
---
+++ src/day-9/src/part_1.rs
@@ -0,0 +1,37 @@
+pub fn solve(lines: &Vec<String>) -> i64 {
+    lines
+        .iter()
+        .fold(Vec::<i64>::new(), |mut acc, line| {
+            let mut nums = line
+                .split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
+
+            let mut last_nums: Vec<i64> = vec![nums.last().unwrap().to_owned()];
+
+            loop {
+                let part_1 = nums[0..nums.len() - 1].to_vec();
+                let part_2 = nums[1..nums.len()].to_vec();
+
+                nums =
+                    part_1
+                        .iter()
+                        .zip(part_2.iter())
+                        .fold(Vec::<i64>::new(), |mut acc, (a, b)| {
+                            acc.push(b - a);
+                            acc
+                        });
+
+                if nums.iter().all(|&num| num == 0) {
+                    break;
+                }
+
+                last_nums.push(nums.last().unwrap().to_owned());
+            }
+
+            acc.push(last_nums.iter().sum());
+            acc
+        })
+        .iter()
+        .sum()
+}
